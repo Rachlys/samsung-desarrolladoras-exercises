54c20c8512857a5f16e062ac7051e1eb4ba936b0
Rachlys <99412200+Rachlys@users.noreply.github.com>
2023-04-25 01:16:27 +0200

    add

--- video_api_html/js/app.js
+++ video_api_html/js/app.js
@@ -2,6 +2,8 @@
 const video_input = document.getElementById('video_input');
 const reproductor_video = document.getElementById('reproductor_video');
 const reproductor_error = document.getElementById('reproductor_error');
+const buttons = document.querySelector('.buttons')
+
 
 video_input.addEventListener('change', (event) => {
     const file = event.target.files[0];
@@ -9,21 +11,46 @@ video_input.addEventListener('change', (event) => {
     
     if (type.startsWith('video/')) {
         const fileURL = URL.createObjectURL(file);
-
+        buttons.style.display = "flex"
         reproductor_error.style.display = "none"
         reproductor_loading.style.display = 'block';
         reproductor_video.style.display = 'none';
         reproductor_video.src = fileURL;
 
         reproductor_video.addEventListener('canplaythrough', () => {
-            document.getElementsByTagName('video')[0].volume = 0.1;
             reproductor_loading.style.display = 'none';
             reproductor_video.style.display = 'block';
         });
     } else {
         reproductor_video.style.display = 'none';
+        buttons.style.display = "none"
         reproductor_video.src = "";
         reproductor_error.style.display = "flex"
     }
     
 });
+
+
+function buttonPlay(){
+    reproductor_video.play()
+}
+
+function buttonStop(){
+    reproductor_video.pause()
+}
+
+function subirVolumen(){
+    if(reproductor_video.volume < 1){
+        reproductor_video.volume += 0.2
+    }else{
+        reproductor_video.volume = reproductor_video.volume
+    }
+}
+
+function bajarVoluemn(){
+    if(reproductor_video.volume > 0.1){
+        reproductor_video.volume -= 0.2
+    }else{
+        reproductor_video.volume = reproductor_video.volume
+    }
+}
